351e87c174462e34822034c30910588daee20a14
Max <maxeust@gmail.com>
2020-11-10 18:03:18 +0100

    working commit

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -1,23 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Deta } from 'deta';
 
-const deta = Deta('');
+const deta = Deta(process.env.DETA_PROJECT_KEY);
 
 const base = deta.Base('todos');
 
 export default async (req, res) => {
-  // const response = await base.put("yo");
-  // console.log(response);
+
   let { body, method } = req;
   let respBody = {};
+
   if (method === 'GET') {
+
     const {value: items} = await base.fetch([]).next();
     respBody = items;
+    res.statusCode = 200;
+
   } else if (method === 'POST') {
+
     body = JSON.parse(body);
     body.isCompleted = false;
     respBody = await base.put(body);
+    res.statusCode = 201;
+
   } 
-  res.statusCode = 200;
+
   res.json(respBody);
 }
